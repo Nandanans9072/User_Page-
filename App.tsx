9bda71e062a86f445cd05cf23cbb6ed5f6df8d69
Nandana Suresh v <96772940+Nandanans9072@users.noreply.github.com>
2023-10-14 17:16:49 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import { memo } from 'react';
+import type { FC } from 'react';
+
+import classes from './App.module.css';
+import resets from './components/_resets.module.css';
+import { Group19 } from './components/Group19/Group19.js';
+
+interface Props {
+  className?: string;
+}
+export const App: FC<Props> = memo(function App(props = {}) {
+  return (
+    <div className={`${resets.clapyResets} ${classes.root}`}>
+      <Group19 />
+    </div>
+  );
+});
